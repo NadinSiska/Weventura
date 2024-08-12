63097f260193f438c60c37714ee86d93e067bf0f
dimas551 <ahmaddimas5151@gmail.com>
2024-08-12 15:15:30 +0700

    FIX ALL BUG

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,67 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.auth')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title', 'Login')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('content')
+
+<section class="contact-us__area section pt-100 section-space-bottom overflow-hidden">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="contact-us__widget mb-30" data-tilt>
+                    <img src="auth/imgs/concact/contact-left-img.png" alt="img not found">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="contact-us__title-wrapper">
+                    <div class="section__title-wrapper mb-40">
+                        <h2 class="section__title-wrapper-title wow fadeInLeft animated" data-wow-delay=".3s">Login</h2>
+                    </div>
+
+                    <div class="contact-us__form-wrapper">
+                        <form class="contact-us__form" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="contact-us__input wow fadeInLeft animated" data-wow-delay=".6s">
+                                        <span>Email</span>
+                                        <input name="email" id="email" type="email" placeholder="Email"
+                                            aria-label="Email" required>
+                                        @error('email')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                        <div class="icon">
+                                            <i class="fa-solid fa-paper-plane"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="contact-us__input wow fadeInLeft animated" data-wow-delay=".6s">
+                                        <span>Password</span>
+                                        <input name="password" id="password" type="password" placeholder="Password"
+                                            aria-label="Password" required>
+                                        @error('password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                        <div class="icon">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="contact-btn mt-30 wow fadeInLeft animated"
+                                        data-wow-delay=".8s">Login</button>
+                                </div>
+                                <div class="col-12 mt-20">
+                                    <p class="text-center">Don't have an account? <a
+                                            href="{{ route('register') }}">Register here</a></p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</section>
+@endsection
